8e61961c0737da62ce444d69c248848e6082974a
Maria-Lisa <lisaloyce16@gmail.com>
2022-04-08 18:34:57 +0000

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - print wit puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str1[100] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str1);
 	return (0);
 }
