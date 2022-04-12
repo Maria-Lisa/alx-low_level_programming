4bc2448386f68d04c47d9efb4bd0d0a81a7069ed
Maria-Lisa <lisaloyce16@gmail.com>
2022-04-12 01:34:35 +0000

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include <ctype.h>
+
 /**
- * main - putchar
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
-	{
-		putchar(tolower(x));
-	}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 	return (0);
 }
